6a7485a83aa7845f3a9065793e5d940501b3e3f2
gabrielcorado <gabrielcorado@mail.com>
2016-05-19 10:59:35 -0300

    Adding Cache support to the Grape formatter

--- lib/venduitz/grape.rb
+++ lib/venduitz/grape.rb
@@ -4,14 +4,21 @@ module Venduitz
   module Grape
     # Call method
     def self.call(object, env)
-      # Define the Venduitz representer
-      representer = env['api.endpoint'].options.fetch(:route_options, {})[:venduitz]
+      # Route Options
+      options = env['api.endpoint'].options.fetch :route_options, {}
 
       # Must define the Representer in the endpoint
-      raise 'Must define the Venduitz in the endpoind' if representer.nil?
+      raise 'Must define the Venduitz in the endpoind' if options[:venduitz].nil?
+
+      # Representer options
+      options[:exclude] ||= []
+
+      # Define the Cache options
+      options[:cache] ||= Venduitz::Cache.enabled
+      options[:cache_options] ||= Venduitz::Cache.config
 
       # Render it!
-      representer.to_json object
+      options[:venduitz].to_json object, options[:exclude], options[:cache], options[:cache_options]
     end
   end
 end
